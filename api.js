adc58da870f45b51dc56e4a93f2ba5d285417dde
Vince <45806875+vince1444@users.noreply.github.com>
2022-02-05 00:04:54 -0500

    Added argument checking for each function

--- api.js
+++ api.js
@@ -1,11 +1,10 @@
-//imports
 const http2 = require('http2');
 const https = require('https');
 
-//players array that holds players
+// players array that holds players
 let players = [];
 
-//creates an account
+// creates an account
 function createAccount(email, password, platform) {
     return {
         email: email,
@@ -14,13 +13,16 @@ function createAccount(email, password, platform) {
     }
 }
 
-//creates a session
+// creates a session
 function createSession(account) {
-    //startDate() and endDate() fill the params for the subsequent requests. By default, I made it pull statistics from 2 months.
-    //If you want to change the stat range, just change the months/days values in these functions.
+    if (account === undefined) {
+        console.error('Missing arguments. Requires: createSession(account).');
+    }
+    // startDate() and endDate() fill the params for the subsequent requests. By default, I made it pull statistics from 2 months.
+    // If you want to change the stat range, just change the months/days values in these functions.
     function startDate() {
         let startDate = new Date();
-        //Subtracts 2 months from current date.
+        // Subtracts 2 months from current date.
         let months = 2;
         startDate.setMonth(startDate.getMonth() - months);
         startDate = startDate.toISOString().split('T')[0].replace(/-/g,'');
@@ -29,14 +31,14 @@ function createSession(account) {
 
     function endDate() {
         let endDate = new Date();
-        //Subtracts 1 day from the current date.
+        // Subtracts 1 day from the current date.
         let days = 1;
         endDate.setDate(endDate.getDate() - days);
         endDate = endDate.toISOString().split('T')[0].replace(/-/g,'');
         return endDate;
     }
 
-    //these values are static for now; change when Ubisoft makes a change to them
+    // these values are static for now; change when Ubisoft makes a change to them
     let appId = '3587dcbb-7f81-457c-9781-0e3f29f6f56a';
     let spaceId = '5172a557-50b5-4665-b7db-e3f2e8c5041d';
     let options = {
@@ -58,11 +60,11 @@ function createSession(account) {
                 let data = '';
                 res.on('error', e => {
                     throw new Error(e);
-                })
+                });
 
                 res.on('data', chunk => {
                     data += chunk;
-                })
+                });
 
                 res.on('end', () => {
                     data = JSON.parse(data);
@@ -75,18 +77,22 @@ function createSession(account) {
                         endDate: endDate(),
                     }
                     resolve(session);
-                })
-            })
+                });
+            });
             req.end();
         }
         catch (e) {
             reject(e);
         }
-    })
+    });
 }
 
-//for troubleshooting session response
+// for troubleshooting session response
 function getSessionResponse(account) {
+    if (account === undefined) {
+        console.error('Missing arguments. Requires: getSessionResponse(account).');
+        return;
+    }
     let appId = '3587dcbb-7f81-457c-9781-0e3f29f6f56a';
     let options = {
         host: 'public-ubiservices.ubi.com',
@@ -107,28 +113,31 @@ function getSessionResponse(account) {
                 let data = '';
                 res.on('error', e => {
                     throw new Error(e);
-                })
+                });
 
                 res.on('data', chunk => {
                     data += chunk;
-                })
+                });
 
                 res.on('end', () => {
                     data = JSON.parse(data);
                     resolve(data);
-                })
-            })
+                });
+            });
             req.end();
         }
         catch (e) {
             reject(e);
         }
-    })
+    });
 }
 
-
-//creates player and adds to global player[] array
+// creates player and adds to global player[] array
 function createPlayer(username, platform, session) {
+    if (username === undefined || platform === undefined || session === undefined) {
+        console.error('Missing arguments. Requires: createPlayer(username, platform, session).');
+        return;
+    }
     let options = {
         host: 'public-ubiservices.ubi.com',
         port: 443,
@@ -148,38 +157,46 @@ function createPlayer(username, platform, session) {
                 let data = '';
                 res.on('error', e => {
                     throw new Error(e);
-                })
+                });
 
                 res.on('data', chunk => {
                     data += chunk;
-                })
+                });
 
                 res.on('end', () => {
                     data = JSON.parse(data);
-                    let id = data.profiles[0].profileId;
-                    let player = {
-                        username: username,
-                        platform: platform,
-                        id: id,
-                        rank: null,
-                        kills: null,
-                        deaths: null,
+                    if (!(data.profiles[0] == undefined)) {
+                        let id = data.profiles[0].profileId;
+                        let player = {
+                            username: username,
+                            platform: platform,
+                            id: id,
+                            rank: null,
+                            kills: null,
+                            deaths: null,
+                        }
+                        players.push(player);
+                        resolve(player);
                     }
-                    //pushes to global players[] array
-                    players.push(player);
-                    resolve(player);
-                })
-            })
+                    else {
+                        reject({ 'Error': 'Player not found.' });
+                    }
+                });
+            });
             req.end();
         }
         catch (e) {
             reject(e);
         }
-    })
+    });
 }
 
-//gets profile server response
+// Platforms: PlayStation = psn, Xbox = xbl, PC = uplay
 function getProfileResponse(username, platform, session) {
+    if (username === undefined || platform === undefined || session === undefined) {
+        console.error('Missing arguments. Requires: getProfileResponse(username, platform, session).');
+        return;
+    }
     let options = {
         host: 'public-ubiservices.ubi.com',
         port: 443,
@@ -199,27 +216,32 @@ function getProfileResponse(username, platform, session) {
                 let data = '';
                 res.on('error', e => {
                     throw new Error(e);
-                })
+                });
 
                 res.on('data', chunk => {
                     data += chunk;
-                })
+                });
 
                 res.on('end', () => {
                     data = JSON.parse(data);
+                    data = data.profiles[0];
                     resolve(data);
-                })
-            })
+                });
+            });
             req.end();
         }
         catch (e) {
             reject(e);
         }
-    })
+    });
 }
 
-//gets general stats of player(rank, kills, deaths, etc)
+// gets general stats of player (rank, kills, deaths, etc)
 function getStats(player, session) {
+    if (player === undefined || session === undefined) {
+        console.error('Missing arguments. Requires: getStats(player, session).');
+        return;
+    }
     let options = {
         host: 'public-ubiservices.ubi.com',
         port: 443,
@@ -240,28 +262,31 @@ function getStats(player, session) {
                 let data = '';
                 res.on('error', e => {
                     reject(e);
-                })
+                });
 
                 res.on('data', chunk => {
                     data += chunk;
-                })
+                });
 
                 res.on('end', () => {
                     data = JSON.parse(data);
                     resolve(data);
-                })
-            })
+                });
+            });
             req.end();
         }
         catch (e) {
             reject(e);
         }
-    })
+    });
 }
 
-//gets stats by season
-//look up season chart in docs
+// look up season chart in docs
 function getStatsBySeason(player, session, season) {
+    if (player === undefined || session === undefined || season === undefined) {
+        console.error('Missing arguments. Requires: getStatsBySeason(player, session, season).');
+        return;
+    }
     let options = {
         host: 'public-ubiservices.ubi.com',
         port: 443,
@@ -282,31 +307,34 @@ function getStatsBySeason(player, session, season) {
                 let data = '';
                 res.on('error', e => {
                     reject(e);
-                })
+                });
 
                 res.on('data', chunk => {
                     data += chunk;
-                })
+                });
 
                 res.on('end', () => {
                     data = JSON.parse(data);
                     let seasonSkillRecord = data.seasons_player_skill_records[0].regions_player_skill_records[0].boards_player_skill_records[0].players_skill_records;
                     resolve(seasonSkillRecord);
-                })
-            })
+                });
+            });
             req.end();
         }
         catch (e) {
             reject(e);
         }
-    })
+    });
 }
 
-//gets stats by operator
-//team param is optional
+// team param is optional
 function getStatsByOperator(player, session, team) {
+    if (player === undefined || session === undefined) {
+        console.error('Missing arguments. Requires: getStatsByOperator(player, session, [OPTIONAL]team).');
+        return;
+    }
     team = (team !== undefined) ? team : 'attacker,defender';
-    //generates an expiration for expiration header
+    // generates an expiration for expiration header
     function genExpiration() {
         let time = new Date();
         //1 hour -- set to what you like. Longer time = less frequent player updates
@@ -332,35 +360,33 @@ function getStatsByOperator(player, session, team) {
     const authority = `https://r6s-stats.ubisoft.com/v1/current/operators/${player.id}?gameMode=all,ranked,casual,unranked&platform=PC&teamRole=attacker,defender&startDate=${session.startDate}&endDate=${session.endDate}`;
     return new Promise((resolve, reject) => {
         try {
-            
             const client = http2.connect(authority);
             let data = '';
             client.on('error', e => {
                 reject(e);
-            })
+            });
             
             const req = client.request(options);
             req.on('error', e => {
                 reject(e);
-            })
+            });
 
             req.on('data', chunk => {
                 data += chunk;
-            })
+            });
 
             req.on('end', () => {
                 data = JSON.parse(data);
                 client.close();
                 const { teamRoles } = data.platforms.PC.gameModes.ranked;
                 resolve(teamRoles);
-            })
+            });
             req.end();
         }
         catch (e) {
             reject(e);
         }
-    })
+    });
 }
 
-//exports
 module.exports = { createAccount, createSession, getSessionResponse, createPlayer, getProfileResponse, getStats, getStatsBySeason, getStatsByOperator, players };
